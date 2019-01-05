dd8b0859556466f311a20538a5a71287096fdd1b
syamjithas <syamjithas@gmail.com>
2019-01-05 09:57:57 +0400

    react

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./load.css";
-//import App from "./App";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-//import Counters from "./components/counters";
-import Calender from "./components/calendar";
 
-ReactDOM.render(<Calender />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
